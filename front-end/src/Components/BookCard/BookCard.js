5146d5b328f993c4a9082800c6bc4fd0a2406761
carla <cfpcarla@gmail.com>
2020-04-22 18:43:08 -0400

    Eslint

--- front-end/src/Components/BookCard/BookCard.js
+++ front-end/src/Components/BookCard/BookCard.js
@@ -1,71 +1,71 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+const useStyles = makeStyles(theme => ({
   icon: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 6)
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4)
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   cardContent: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
+    padding: theme.spacing(6)
+  }
 }));
 
 export default function BookCard(props) {
   const classes = useStyles();
 
   return (
-    <React.Fragment >
-      <Card className={classes.card} >
-        {/* <form onSubmit={handleSubmit}> */}
-        <CardMedia
-          className={classes.cardMedia}
-          image="https://s26162.pcdn.co/wp-content/uploads/2018/12/184224_1326895.794x1200_q95_crop-smart_upscale.jpg"
-          title="Image title"
-        />
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
-          <Typography>
-          {props.author}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-        </CardActions>
-        {/* </form> */}
-      </Card>
+    <React.Fragment>
+      <Grid>
+        <Card className={classes.card}>
+          {/* <form onSubmit={handleSubmit}> */}
+          <CardMedia
+            className={classes.cardMedia}
+            image="https://s26162.pcdn.co/wp-content/uploads/2018/12/184224_1326895.794x1200_q95_crop-smart_upscale.jpg"
+            title="Image title"
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.title}
+            </Typography>
+            <Typography>{props.author}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              View
+            </Button>
+          </CardActions>
+          {/* </form> */}
+        </Card>
+      </Grid>
     </React.Fragment>
-  )
+  );
 }
--- front-end/src/Components/BookCardList/BookCardList.js
+++ front-end/src/Components/BookCardList/BookCardList.js
@@ -2,39 +2,6 @@ import React from 'react';
 import BookCard from '../BookCard/BookCard'
 import Grid from '@material-ui/core/Grid';
 import Async from 'react-async';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
 
 const loadBooks = () =>
   fetch("http://localhost:8080/books")
@@ -42,7 +9,6 @@ const loadBooks = () =>
     .then(res => res.json())
 
 export default function BookCardList() {
-  const classes = useStyles();
   return (
     <Async promiseFn={loadBooks}>
       <Async.Loading>Loading...</Async.Loading>
@@ -51,7 +17,7 @@ export default function BookCardList() {
           return (
             <div>
               {data.books.map(bookCard => (
-                <Grid item key={bookCard.id} xs={12} sm={6} md={4}>
+                <Grid item key={bookCard.id}  xs={12} sm={6} md={4}>
                   <BookCard
                   title={bookCard.title}
                   author={bookCard.author} />
